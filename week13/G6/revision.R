bc5ed0bbd86c0ac3169049ab64ee5de77610c921
Martin老師 <tpemartin@gmail.com>
2022-12-12 09:10:24 +0800

    Update revision.R

--- week13/G6/revision.R
+++ week13/G6/revision.R
@@ -8,6 +8,7 @@ tidyr::pivot_longer(data=Accident,cols=2:11,
              values_to="NumberOfAccident")->Accident
 Accident$city<-factor(Accident$city,levels = c("臺北市","新北市","桃園市","臺中市","臺南市","高雄市"))
 
+# Original design -------
 library(ggplot2)
 plt = econDV2::Plot()
 plt$ggplot = ggplot(data=Accident,
@@ -27,25 +28,27 @@ plt$others = list(
           axis.title.y = element_text(size = 15),
           legend.key.size = unit(30,"pt"))
 )
+plt$scale = scale_colour_manual(values = c("#FF9900","#e9e9e9",
+                                           "#CCCCCC","#999999","#666666","#333333"))
 
-plt$make()+
-  scale_colour_manual(values = c("#FF9900","#e9e9e9",
-                                 "#CCCCCC","#999999","#666666","#333333"))
+plt$make()
 
-martinStyle2::Style2() -> sty
-plt$make()+
-  scale_colour_manual(values = c("#FF9900","#557153",
-                                 "#7D8F69","#CEAB93","#A9AF7E","#AD8B73"))+
-  sty$theme()+
-  ylab(NULL)+xlab(NULL)
 
+# Revision with color twist -------
 plt2 = plt
-plt2$others2 = list(
+plt2$scale = 
   scale_colour_manual(values = c("#FF9900","#557153",
-                                 "#7D8F69","#CEAB93","#A9AF7E","#AD8B73")),
-    sty$theme(),
-    ylab(NULL), xlab(NULL)
-)
+                                 "#7D8F69","#CEAB93","#A9AF7E","#AD8B73")) 
+plt2$make()
+
+
+# Makeup -----
+sty = martinStyle2::Style2()
+
+plt2$others2 = list(
+  sty$theme(),
+  ylab(NULL), xlab(NULL))
+
 plt2$make()
 
 
